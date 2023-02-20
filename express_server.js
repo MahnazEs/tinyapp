b4b152a02247f21616c8acc8c7d0a364cda3b447
“MahnazEs” <esmaeilymahnaz@gmail.com>
2023-02-20 15:46:39 -0800

    sending html to server

--- express_server.js
+++ express_server.js
@@ -7,12 +7,22 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+
+//http://localhost:8080/
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
+
+//http://localhost:8080/urls.json
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
+});
+
+
+//http://localhost:8080/hello
+app.get("/hello", (req, res) => {
+  res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
 
