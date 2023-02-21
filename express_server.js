6ba838de9e81acd5dd7e46b7fd09a432b62db82c
“MahnazEs” <esmaeilymahnaz@gmail.com>
2023-02-20 21:47:06 -0800

    add urls_new and use body

--- express_server.js
+++ express_server.js
@@ -3,6 +3,11 @@ const app = express();
 const PORT = 8080; // default port 8080
 
 
+//translate body
+app.use(express.urlencoded({ extended: true }));
+
+
+
 //set ejs
 app.set("view engine", "ejs");
 
@@ -25,6 +30,21 @@ app.get("/hello", (req, res) => {
   const templateVars = { greeting: "Hello World!" };
   res.render("hello_world", templateVars);
 });
+
+
+//render urls_new.ejs
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+
+
+//get data from the form in body and translate it
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("ok"); // Respond with 'Ok' (we will replace this)
+});
+
 
 
 //sending data to urls_show.ejs
@@ -51,6 +71,10 @@ app.get("/urls.json", (req, res) => {
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
+
+
+
+
 
 
 
